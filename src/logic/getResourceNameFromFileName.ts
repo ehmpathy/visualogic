5b2870a3aed8ea46fdbba2e206f9fe6ce1251867
Ulad Kasach <uladkasach@gmail.com>
2024-12-01 23:32:33 -0800

    fix(name): extract resource name better from tests

--- src/logic/getResourceNameFromFileName.ts
+++ src/logic/getResourceNameFromFileName.ts
@@ -1,2 +1,10 @@
 export const getResourceNameFromFileName = (fileName: string): string =>
-  fileName.split('/').slice(-1)[0]!.split('.').slice(0, -1).join('.');
+  fileName
+    .split('/')
+    .slice(-1)[0]!
+    .split('.')
+    .slice(0, -1)
+    .join('.')
+    .replace('.acceptance.test', '')
+    .replace('.integration.test', '')
+    .replace('.test', '');
